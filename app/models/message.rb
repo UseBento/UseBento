acf297d124780c93612fa20b1868cbdcfdca6b29
David Karn <davidkarnseattle@gmail.com>
2015-01-10 11:28:52 -0800

    Basic formatting and autmatic linking of project messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,9 @@ class Message
   
   belongs_to :user
   embedded_in :project
+
+  def body_as_html
+    body = self.body.gsub(URI.regexp, "<a href='\\0'>\\0</a>")
+    "<p>" + body.split(/(\r?\n){2,}/).join("</p><p>") + "</p>"
+  end
 end
