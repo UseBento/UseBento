2d6b01cd001cf354d6d332c6320959c1918b8bbc
David Karn <davidkarnseattle@gmail.com>
2015-01-28 21:23:38 -0800

    Fix images with dots in the name

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get  'projects/:id/del_project',     to: 'projects#delete'         
   post 'projects/:project_id/message', to: 'messages#post_message'         
   post 'projects/:project_id/update_payment', to: 'projects#update_payment'         
-  get  'attachment/:project_id/:message_id/:attachment_id/:filename', to: 'messages#view_attachment'
+  get  'attachment/:project_id/:message_id/:attachment_id/:filename', to: 'messages#view_attachment', constraints: { :filename => /[^\/]+/ }
 
   root 'welcome#index'
   get  'contact',                     to: 'application#contact'
