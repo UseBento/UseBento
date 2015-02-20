01a6d741333629dc3967e3358e6ed61a633bfc42
Viktoras Truchanovicius <viktoras@bakedandbranded.com>
2015-02-19 22:15:24 -0500

    adding environment file for storing sensitive information

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'li514-238.members.linode.com:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :authentication => :plain,
@@ -8,10 +8,10 @@ Rails.application.configure do
     :port => 587,
     :domain => "sandboxc84676d0390645d2abda73fd1b5115fd.mailgun.org",
     :user_name => "postmaster@sandboxc84676d0390645d2abda73fd1b5115fd.mailgun.org",
-    :password => "95eyypl176p8"
+    :password => ENV['MAILGUN_PW']
   }
 
-  config.twocheckout_private_key = 'CA54A03A-0324-4810-9946-253A028D58C2'
+  config.twocheckout_private_key = ENV['TWOCHECKOUT_PRIVATE_KEY']
   config.twocheckout_public_key  = '7EB9463F-A64C-4CF5-9B49-912A9912E3D7'
   config.twocheckout_seller_id   = 901261089
   config.twocheckout_sandbox     = true
