18f891ba3e2427185468150e02bc6ecfe748db00
Viktoras Truchanovicius <viktoras@bakedandbranded.com>
2015-02-19 21:25:47 -0500

    adding mongo config for staging, enabling stack trace in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -42,7 +42,14 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :debug
+  config.log_level = :info
+
+  config.consider_all_requests_local       = true
+  
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
