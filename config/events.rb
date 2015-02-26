b18b297663b3afa1ff5444c81a8d1671391645c1
David Karn <davidkarnseattle@gmail.com>
2015-02-26 11:31:43 -0800

    implementing authorization of websocket channesl

--- config/events.rb
+++ config/events.rb
@@ -1,4 +1,25 @@
 WebsocketRails::EventMap.describe do
+  namespace :chat do
+      subscribe :client_connected, :to => ChatController, :with_method => :connected
+  end 
+
+
+  class AuthorizationController < WebsocketRails::BaseController
+    def authorize
+      channel      = WebsocketRails[message[:channel]]
+      project_id   = channel.split(":")[1]
+      project      = Project.find(project_id)
+
+      if project && project.user == current_user
+        accept_channel current_user
+      else
+        deny_channel({:message => 'authorization failed!'})
+      end
+    end
+  end
+      
+  subscribe :subscribe, :to => AuthorizationController, :with_method => :authorize
+
   # You can use this file to map incoming events to controller actions.
   # One event can be mapped to any number of controller actions. The
   # actions will be executed in the order they were subscribed.
@@ -11,4 +32,5 @@ WebsocketRails::EventMap.describe do
   #     subscribe :new, :to => ProductController, :with_method => :new_product
   #   end
   # The above will handle an event triggered on the client like `product.new`.
+
 end
